c85b07f1bf35ff8729cc09ded003a88479bc91e4
Leon Zhang <jzhang867@wisc.edu>
2018-12-04 22:11:00 -0600

    MealListModifiedByLeon

--- src/MealList.java
+++ src/MealList.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MealList {
 
+    private List<FoodItem> mealList;
+    private HashMap<String, Double> nutritionSummary;
+
+    public MealList() {
+        mealList = new ArrayList<>();
+        nutritionSummary = new HashMap<>();
+    }
+
+    //adds a food item to MealList
+    public void addItem(FoodItem foodItem) {
+        if (foodItem == null) throw new NullPointerException();
+        mealList.add(foodItem);
+    }
+
+    //removes a food item from MealList
+    public void removeItem(FoodItem foodItem) {
+        if (foodItem == null) throw new NullPointerException();
+        if (mealList.isEmpty()) return;
+        mealList.remove(mealList.indexOf(foodItem));
+    }
+
+    //traverses the entire list and adds up all nutrition
+    public void analyzeMeal() {
+
+    }
+
+    //return the current MealList
+    public List<FoodItem> getMealList() {
+        return mealList;
+    }
 }
